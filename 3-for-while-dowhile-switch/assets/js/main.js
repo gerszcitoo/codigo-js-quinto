8dbcecc229c9ab3718707d5e8df72d283a93357e
German Sanchez <germands2013>
2023-05-17 00:19:48 -0300

    correcciones generales a la clase 3

--- 3-for-while-dowhile-switch/assets/js/main.js
+++ 3-for-while-dowhile-switch/assets/js/main.js
@@ -1,6 +1,6 @@
 // ----------For-----------
 /* for (let i = 10; i > 0; i--) {
-    console.log(i);
+  console.log(i);
 }
 console.log("Sigue el código"); */
 
@@ -13,81 +13,82 @@ for (i = 1; i <= 10; i++) {
     console.log(ingresarNumero + " x10 " + i + " = " + resultado);
 } */
 
-// ----------Turnos con for------
+// ----------Turnos con for----------
 /* for (let i = 1; i <= 20; i++) {
-    let nombre = prompt("Ingrese su nombre");
-    console.log("Turno N°: " + i + "\nNombre: " + nombre);
+  let nombre = prompt("Ingrese su nombre");
+  alert("Turno N°: " + i + "\nNombre: " + nombre);
 } */
 
 // --------Uso del break y continue-----------
 /* for (let i = 0; i < 10; i++) {
-    if (i == 5) {
-        break;
-    }
-    console.log(i);
+  if (i == 5) {
+    break;
+  }
+  console.log(i);
 }
 
 for (let i = 0; i < 10; i++) {
-    if (i == 5) {
-        continue;
-    }
-    console.log(i);
+  if (i == 5) {
+    continue;
+  }
+  console.log(i);
 } */
 
 // --------Recorrer Array dinámico (adelanto)--------
-/* let nombres = ["Ger", "Maxi", "Hernán", "Matías"];
+/* let nombres = ["Germán", "Maxi", "Joaquín", "Martina"];
 for (let i = 0; i < nombres.length; i++) {
-    console.log(nombres[i]);
+  console.log(nombres[i]);
 } */
 
 // -------------While----------
 /* let repetir = true;
 while (repetir) {
-    console.log("Al infinito y más allá");
+  console.log("Al infinito y más allá");
 } */
 
-/* let entrada = prompt("Ingresar un dato (Para salir escriba esc)");
+/* let entrada = prompt("Ingrese un dato (Para salir escriba esc)");
 while (entrada != "esc") {
-    console.log(entrada);
-    entrada = prompt("Ingrese una entrada (Para salir escriba esc)");
+  console.log(entrada);
+  entrada = prompt("Ingrese un dato (Para salir escriba esc)");
 } */
 
 // ---------Do While---------
 /* let repetir = false;
 do {
-    console.log("solo una vez");
+  console.log("solo una vez");
 } while (repetir); */
 
 /* let numero = 0;
 do {
-    numero = prompt("Ingrese un número");
-    if (numero != "esc") {
-        alert(numero);
-    } else {
-        alert("Usted salió del bucle");
-    }
+  numero = prompt("Ingrese un número (Para salir escriba esc)");
+  if (numero != "esc") {
+    alert(numero);
+  } else {
+    alert("Usted salió del bucle");
+  }
 } while (numero != "esc"); */
 
 // ------------Switch----------
-/* let nombre = prompt("Ingrese su nombre");
+let nombre = prompt("Ingrese su nombre (Para salir escriba esc)");
+nombre = nombre.toLowerCase();
 while (nombre != "esc") {
-    switch (nombre) {
-        case "ana":
-            console.log("El DNI de Ana es 14823294");
-            break;
-        case "marco":
-            console.log("El DNI de Marcos es 33519294");
-            break;
-        case "ariel":
-            console.log("El DNI de Ariel es 33519015");
-            break;
-        default:
-            console.log("No tenemos ese dato");
-            break;
-    }
-    nombre = prompt("Ingrese su nombre");
-
-} */
+  switch (nombre) {
+    case "ana":
+      alert("El DNI de Ana es 14823294");
+      break;
+    case "marcos":
+      alert("El DNI de Marcos es 33519294");
+      break;
+    case "ariel":
+      alert("El DNI de Ariel es 33519015");
+      break;
+    default:
+      alert("No tenemos ese dato");
+      break;
+  }
+  nombre = prompt("Ingrese su nombre (Para salir escriba esc)");
+  nombre = nombre.toLowerCase();
+}
 
 // -----------Calculadora------------
 /* let numero1, numero2, operacion, continuar;

6fd3ee3f381387220962a92f11e9fd0ce1cd81ce
German Sanchez <germands2013>
2023-05-19 10:32:06 -0300

    adiciones a clase 4

--- 4-funciones/assets/js/main.js
+++ 4-funciones/assets/js/main.js
@@ -2,6 +2,7 @@
 /* function saludar() {
   console.log("Hola estudiantes!");
 }
+
 saludar(); */
 
 // ----------Ejemplo de funciones-------
@@ -15,10 +16,9 @@ for (let i = 0; i <= 4; i++) {
 } */
 
 // ---------Función con parámetros--------
-
 /* function conParametros(parametro1, parametro2) {
   console.log(
-    `El parámetro 1 es: ${parametro1} \nEl parámetro 2 es: ${parametro2} `
+    `El parámetro 1 es: ${parametro1} \nEl parámetro 2 es: ${parametro2}`
   );
 }
 
@@ -29,15 +29,12 @@ conParametros(12, 15); */
 // --------Uso de Return--------
 
 /* let resultado = 0;
-
 function suma(numero1, numero2) {
   return numero1 + numero2;
 }
-
 function mostrar(mensaje) {
   console.log(mensaje);
 }
-
 resultado = suma(12, 3);
 mostrar(resultado); */
 
@@ -52,18 +49,16 @@ mostrar(resultado); */
     case "-":
       return numero1 - numero2;
     case "*":
-      return numero1 - numero2;
+      return numero1 * numero2;
     case "/":
-      return numero1 - numero2;
+      return numero1 / numero2;
   }
 }
 console.log(calculadora(12, 12, "+")); */
 
 // ---------Ejemplo variables globales y locales----------
-
 /* // variable global
 let resultado = 0;
-
 function sumar(primerNumero, segundoNumero) {
   // variable local
   let resultado;
@@ -86,21 +81,22 @@ function practica() {
 practica(); */
 
 // -----------Funciones anónimas-----------
-/* // no tienen nombre, se suelen almacenar en constantes
-const SUMA = function (a, b) {
+// no tienen nombre, se suelen almacenar en constantes
+/* const SUMA = function (a, b) {
   console.log(a + b);
 };
 // SUMA(5, 5);
 // console.log(SUMA);
 const RESTA = function (a, b) {
   return a - b;
-}; */
+};
+// RESTA(8, 2);
+console.log(RESTA); */
 
 // -------------Función flecha-----------
-/* // si es en misma linea no necesita return ni llaves
-const SUMA = (a, b) => a + b;
+// si es en misma linea no necesita return ni llaves
+/* const SUMA = (a, b) => a + b;
 const RESTA = (a, b) => a - b;
-
 // si tiene un solo parametro se pueden obviar los parentesis
 const IVA = (a) => a * 0.21;
 let precioProducto = 500;
@@ -109,6 +105,8 @@ let precioFinal = RESTA(SUMA(precioProducto, IVA(precioProducto)), descuento);
 console.log(precioFinal); */
 
 // -------------Adelanto----------
-/* 
-let saludar = document.getElementById("saludar");
-saludar.addEventListener("click", function() { console.log("hola") }); */
+
+/* let saludar = document.getElementById("saludar");
+saludar.addEventListener("click", function () {
+  alert("hola");
+}); */

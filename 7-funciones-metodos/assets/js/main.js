c8640ee8a75e1c53de4db59f9a04c5aafdb8a195
German Sanchez <germands2013>
2023-06-16 09:55:15 -0300

    clase 7 y 8

--- 7-funciones-metodos/assets/js/main.js
+++ 7-funciones-metodos/assets/js/main.js
@@ -1,7 +1,7 @@
 // -----------Funciones de orden superior-----------
 // -----------Función que retorna función-----------
 /* function mayorQue(numero) {
-    return (numeroaEvaluar) => numeroaEvaluar > numero;
+  return (numeroaEvaluar) => numeroaEvaluar > numero;
 }
 
 let mayorQueDiez = mayorQue(10);
@@ -12,9 +12,9 @@ console.log(mayorQueCien(200)); */
 
 // ----------Recibir funciones por parámetro---------
 /* function porCadaElemento(array, funcion) {
-    for (const elemento of array) {
-        funcion(elemento);
-    }
+  for (const elemento of array) {
+    funcion(elemento);
+  }
 }
 
 let nombres = ["Juan", "María", "Mariano", "Patricia"];
@@ -26,23 +26,23 @@ porCadaElemento(numeros, console.log); */
 /* let total = 0;
 
 function acumular(num) {
-    total += num;
-    console.log(total);
-    return total;
+  total += num;
+  console.log(total);
+  return total;
 }
-// console.log(porCadaElemento(numeros, acumular)); */
+console.log(porCadaElemento(numeros, acumular)); */
 
 // --------------Con funcion anonima-----------
 /* function porCadaElemento(array, funcion) {
-    for (const elemento of array) {
-        funcion(elemento);
-    }
+  for (const elemento of array) {
+    funcion(elemento);
+  }
 }
 let numeros = [4, 6, 9, 2];
 
 const numerosPor2 = [];
 porCadaElemento(numeros, (el) => {
-    numerosPor2.push(el * 2);
+  numerosPor2.push(el * 2);
 });
 
 console.log(numerosPor2); */
@@ -50,65 +50,82 @@ console.log(numerosPor2); */
 // -------------Métodos de búsqueda y transformación-------------
 
 let listaProductos = [
-    { nombre: "televisor", precio: 1000 },
-    { nombre: "computadora", precio: 2000 },
-    { nombre: "computadora", precio: 3000 },
-    { nombre: "ventilador", precio: 5000 },
-    { nombre: "tostadora", precio: 200 },
-    { nombre: "estufa", precio: 1500 },
+  { nombre: "televisor", precio: 1000 },
+  { nombre: "computadora", precio: 2000 },
+  { nombre: "computadora", precio: 3000 },
+  { nombre: "ventilador", precio: 5000 },
+  { nombre: "tostadora", precio: 200 },
+  { nombre: "estufa", precio: 1500 },
 ];
 
 // ------FOR EACH---------
-/* listaProductos.forEach(producto => {
-    console.log(`Este objeto es ${producto.nombre} y su precio es ${producto.precio}`)
+/* listaProductos.forEach((producto) => {
+  document.write(
+    `Este objeto es ${producto.nombre} y su precio es ${producto.precio}<br>`
+  );
 }); */
 
 // -------FIND (sólo trae el PRIMERO que encuentre, trae OBJETO)--------
 /* let busquedaUsuario = prompt("Qué desea buscar?");
-let buscar = listaProductos.find(producto => producto.nombre == busquedaUsuario);
-console.log(buscar); */
+let buscar = listaProductos.find(
+  (producto) => producto.nombre == busquedaUsuario
+);
+
+if (buscar == undefined) {
+  document.write("No se encontró el producto");
+} else {
+  document.write`${JSON.stringify(buscar)}`;
+} */
 
 // ---------FILTER (devuelve todos los que enecuentra, trae ARRAY)---------
-/* let buscaPrecio = listaProductos.filter(prod => prod.precio > 2000);
-
-console.log(buscaPrecio); */
+/* let buscaPrecio = listaProductos.filter((prod) => prod.precio > 2000);
+document.write(JSON.stringify(buscaPrecio)); */
 
 // ------------SOME (devuelve un booleano)----------
-/* let existeProducto = listaProductos.some(prod => prod.nombre == "computadora");
-console.log(existeProducto); */
+/* let existeProducto = listaProductos.some(
+  (prod) => prod.nombre == "computadora"
+);
+document.write`${existeProducto}`; */
 
 // -----------MAP (copiar array)----------
-/* let productosIva = listaProductos.map(prod => prod.precio = prod.precio * 1.21);
-console.log(productosIva); */
+/* let productosIva = listaProductos.map(
+  (prod) => (prod.precio = prod.precio * 1.21)
+);
+document.write`${productosIva}`; */
 
 // -----------REDUCE (sumatoria total)----------
-/* let totalCompra = listaProductos.reduce((acumulador, producto) => acumulador + producto.precio, 0);
-console.log(totalCompra); */
+/* let totalCompra = listaProductos.reduce(
+  (acumulador, producto) => acumulador + producto.precio,
+  0
+);
+document.write`${totalCompra}`; */
 
 // ----------SORT (ordenar)----------
 /* const animales = ["elefante", "perro", "leon", "camello"];
 const numeros = [40, 1, 5, 200];
 console.log(animales.sort());
 console.log(numeros.sort((a, b) => a - b));
-console.log(numeros.sort((a, b) => b - animales));
+console.log(numeros.sort((a, b) => b - a));
 // Ordenar alfabéticamente
-console.log(listaProductos.sort((a, b) => {
+console.log(
+  listaProductos.sort((a, b) => {
     if (a.nombre > b.nombre) {
-        return 1
+      return -1;
     }
     if (a.nombre < b.nombre) {
-        return -1
+      return 1;
     }
     return 0;
-})); */
+  })
+); */
 
 // -----------Objeto MATH (Trabajar con matemática)-----------
-// console.log(Math.E);
+// alert(Math.PI);
 // -----------Objeto DATE (fechas)------------
-/* console.log(new Date(2020, 1, 15));
+/* alert(new Date(2022, 12, 25));
 
 const casiNavidad = new Date(2021, 11, 25, 23, 59, 59);
 console.log(casiNavidad);
 
-const hoy = new Date("December 17, 2021");
+const hoy = new Date("June 14, 2023");
 console.log(hoy.toDateString()); */
---
+++ 8-interaccion/assets/js/main.js
@@ -0,0 +1,106 @@
+// ---------Métodos de acceso a los nodos-----------
+// 1. Por ID - trae un OBJETO
+/* let titulo = document.getElementById("titulo");
+console.log(document); */
+
+// 2. Por clase - trae un ARRAY
+/* console.log(document.getElementsByClassName("parrafo"));
+// Traer un objeto del array
+console.log(document.getElementsByClassName("parrafo")[0]); */
+
+// 3. Por etiqueta - trae un ARRAY
+/* console.log(document.getElementsByTagName("h2"));*/
+
+// 4. Selectores - IGUAL que en CSS - SÓLO el PRIMER OBJETO que encuentra
+/* console.log(document.querySelector(".parrafo"));
+// Traer más de un objeto - trae un ARRAY
+console.log(document.querySelectorAll("#titulo"));
+console.log(document.querySelectorAll("div > form"));
+console.log(document.querySelectorAll(".parrafo"));
+console.log(document.querySelectorAll("p")); */
+
+// ---------Acceder a contenido de etiqueta (innerText)---------
+/* let titulo = document.getElementById("titulo");
+console.log(titulo.innerText);
+titulo.innerText = "Hola Mundo"; */
+
+// --------Ejemplo Array con nodos---------
+/* let listaParrafos = document.querySelectorAll("p");
+
+for (let materia of listaParrafos) {
+  console.log(`Esta materia es ${materia.innerText}`);
+} */
+
+// ------Cambiar contenido de etiqueta-----------
+/* let titulo = document.getElementById("titulo");
+titulo.innerText = "Cambio de texto";
+titulo.innerHTML = "<strong>agrego HTML</strong>"; */
+
+// ------Cambiar CSS desde JS---------
+/* let titulo = document.getElementById("titulo");
+titulo.style.color = "red";
+titulo.style.textAlign = "center";
+titulo.style.display = "none"; */
+
+// ------Agregar Clase---------
+/* let titulo = document.getElementById("titulo");
+titulo.className = "container row"; */
+
+// ------Cambiar atributos-------
+/* let titulo = document.getElementById("titulo");
+titulo.setAttribute("class", "titulo1");
+
+let link = document.querySelector("a");
+link.setAttribute("href", "https://www.google.com");
+link.setAttribute("target", "blank_"); */
+
+// ----------Agregar y sacar HTML-----------
+/* let subtitulo = document.createElement("h2");
+subtitulo.setAttribute("id", "nuevoElemento");
+subtitulo.innerHTML = "<a>Este es un link</a>";
+console.log(subtitulo);
+
+document.getElementById("aca").appendChild(subtitulo);
+document.getElementById("aca").removeChild(subtitulo); */
+
+// -----------Leer Input----------
+/* // escribir input
+document.getElementById("nombre").value = "Nombre";
+// leer input
+console.log(document.getElementById("nombre").value); */
+
+// ---------Puesta en práctica---------
+/* class Producto {
+  constructor(nombre, categoria, precio) {
+    this.nombre = nombre;
+    this.categoria = categoria;
+    this.precio = precio;
+  }
+}
+
+let listaProductos = [
+  { nombre: "televisor", categoria: "electro", precio: 2000 },
+  { nombre: "lavarropas", categoria: "electro", precio: 5000 },
+  { nombre: "tostadora", categoria: "electro", precio: 100 },
+];
+
+const agregarProducto = () => {
+  let nombre = document.querySelector("#nombre").value;
+  // let nombre2 = document.querySelectorAll("#nombre")[0].value;
+  let categoria = document.querySelector("#categoria").value;
+  let precio = parseFloat(document.querySelector("#precio").value);
+
+  let productoNuevo = new Producto(nombre, categoria, precio);
+  listaProductos.push(productoNuevo);
+  return productoNuevo;
+};
+
+listaProductos.forEach((el) => {
+  let nodo = document.createElement("div");
+  nodo.className = "producto";
+  nodo.setAttribute("id", el.nombre);
+  nodo.innerHTML = `<h3>${el.nombre}</h3>
+                    <p>${el.categoria}</p>
+                    <p>${el.precio}</p>`;
+  document.getElementById("productos").appendChild(nodo);
+}); */
